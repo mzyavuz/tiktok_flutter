b27ee3ba954fef901281d22e3644efc418c2a109
mzyavuz <melikezeynep_97@hotmail.com>
2023-01-06 15:41:28 +0300

    first commit

---
+++ lib/app/tiktok_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/app/tiktok_app.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
